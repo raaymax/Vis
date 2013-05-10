d1c64fde058e0c14b104e7ff21d0aed9a6343303
Mateusz Russak <raay.max@gmail.com>
2013-05-10 17:48:37 +0200

    Colors!

--- src/libs/plotter/Plotter.cpp
+++ src/libs/plotter/Plotter.cpp
@@ -1,23 +1,33 @@
 #include "Plotter.h"
 #include <QPainter>
+#include <qpaintengine.h>
 
 Plotter::Plotter(QWidget * parent):
     QWidget(parent)
 {
-    
+    setFixedSize(500,150);
 }
 void Plotter::paintEvent( QPaintEvent* ){
     QPainter p(this);
     int w = width();
     int h = height();
     
+    //p.setBackground(QBrush(Qt::white));
+    QPen pen(Qt::red);
+    pen.setWidth(2);
+    p.setPen(pen);
+    
+    p.setRenderHint(QPainter::Antialiasing);
+    
     foreach(const QList<qreal> &plot , plots){
         qreal x = 0;
         qreal y = 0;
-        qreal step = w/plot.size();
+        qreal step = w/(plot.size()-1);
         foreach(const qreal & r, plot){
-            p.drawLine(x,y,x+step,r);
-            x += step;
+            if(plot.first() != r){
+                p.drawLine(x,h-y,x+step,h-r);
+                x += step;
+            }
             y = r;
         }
     }
